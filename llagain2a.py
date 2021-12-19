71c6baa8e13a28262f1f102b6d913a67f4421eab
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-12-19 16:55:57 +0530

    first commit

---
+++ llagain2a.py
@@ -0,0 +1,85 @@
+class Node:
+    def __init__(self,data):
+        self.data= data
+        self.next= None
+        
+class Llist:
+    def __init__(self):
+        self.head= None
+        
+    def printList(self):
+        temp= self.head
+        
+        while(temp):
+            print(temp.data)
+            temp= temp.next 
+            
+    def push(self,data):
+        
+        newnode= Node(data)
+        newnode.next= self.head
+        self.head= newnode
+        
+    def append(self,data):
+        
+        newnode= Node(data)
+        
+        if self.head==None:
+            
+            self.head =newnode
+            return
+        
+        
+        temp= self.head
+        while temp.next :
+            
+            temp= temp.next
+            
+        temp.next= newnode
+        
+    
+    def insertAfter(self,prev,data):
+        
+        newnode= Node(data)
+        
+        newnode.next= prev.next
+        prev.next= newnode
+        
+    def pop(self):
+        
+        if self.head==None:
+            return None
+
+        temp= self.head
+        self.head= self.head.next
+        temp=None
+        
+        
+        
+        
+        
+            
+            
+            
+        
+        
+            
+                       
+        
+llist= Llist()
+
+llist.head= Node(1)
+second= Node(2)
+third= Node(3)
+
+llist.head.next= second
+second.next= third
+llist.push(9)
+llist.push(7)
+
+llist.append(8)
+llist.insertAfter(llist.head,99)
+llist.pop()
+llist.printList()
+
+
