c7599c81301f4d2ff1f3094c844a56ae2d37787d
apurvaone <57245377+apurvaone@users.noreply.github.com>
2022-01-10 15:54:30 +0530

    binary search recursive

---
+++ bserach_recursuve.py
@@ -0,0 +1,27 @@
+def bsearch_rec(arr,l,r,el):
+    
+    
+    if l>r:
+        return -1
+    
+    mid= (l+r)//2
+    
+    if arr[mid]==el:
+        return mid
+    
+    elif arr[mid]<el:
+        return bsearch_rec(arr,mid+1,r,el)
+        
+    else:
+        return bsearch_rec(arr,l,mid-1,el)
+        
+        
+    
+        
+        
+p= bsearch_rec([1,2,3,4,5,6,7],0,6,7)
+
+print(p)
+    
+    
+     
