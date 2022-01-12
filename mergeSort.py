cebe1f99787af9bfb66b37d0e9e246924ed99927
apurvaone <57245377+apurvaone@users.noreply.github.com>
2022-01-12 19:54:43 +0530

    Merge Sort

---
+++ mergeSort.py
@@ -0,0 +1,39 @@
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+        
+        print(left)
+        print(right)
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    
+    
+arr=[38,27,43,3,9,82,10]
+mergeSort(arr)
+print(arr)
+    
